d6f81ecd08638b6d7e399732f9d1ed03b712e855
James <jamesdownslab@gmail.com>
2020-01-24 15:13:48 +0000

    Added original versions of basics and colors

---
+++ basics.py
@@ -0,0 +1,200 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+__all__ = [
+    'display',
+    'plot',
+    'read_img',
+    'load',
+    'read',
+    'write_img',
+    'save',
+    'write',
+    'get_width_and_height',
+    'dimensions',
+    'get_height',
+    'height',
+    'get_width',
+    'width',
+    'to_uint8',
+    'get_depth',
+    'Displayer',
+    'mean',
+]
+
+
+class Displayer:
+    def __init__(self, window_name, resolution=(1280, 720)):
+        self.window_name = window_name
+        cv2.namedWindow(self.window_name, cv2.WINDOW_KEEPRATIO)
+        cv2.resizeWindow(self.window_name, *resolution)
+
+    def update_im(self, im):
+        cv2.imshow(self.window_name, im)
+        if cv2.waitKey(100) & 0xFF == ord('q'):
+            pass
+
+
+def mean(ims):
+    im = np.stack(ims, axis=len(np.shape(ims[0])))
+    im = np.float32(im)
+    im = np.mean(im, axis=len(np.shape(ims[0])))
+    return np.uint8(im)
+
+def display(image, title='', resolution=(960, 540)):
+    """Uses cv2 to display an image then wait for a button press"""
+    cv2.namedWindow(title, cv2.WINDOW_KEEPRATIO)
+    cv2.resizeWindow(title, *resolution)
+    cv2.imshow(title, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def plot(im):
+    plt.figure()
+    plt.imshow(im)
+    plt.show()
+
+def to_uint8(im):
+    im = (im - np.min(im))/(np.max(im)-np.min(im)) * 255
+    return np.uint8(im)
+
+def read_img(filepath, flag=1):
+    """
+    Reads an image from a filepath.
+
+    The image should be in the working directory or a full path of image
+    should be given.
+
+    Parameters
+    ----------
+    filepath: filepath of the image
+
+    flag: Specifies how the image is read
+        1: Loads a color image. Any transparency will be neglected.
+        0: Loads image in grayscale mode.
+        -1: Loads image including alpha channel
+
+    Returns
+    -------
+    img: output image
+        Number of channels will be determined by the chosen flag.
+        Equal to None if filepath does not exist
+        Color images will have channels stored in BGR order
+
+    """
+    img = cv2.imread(filepath, flag)
+    return img
+
+
+load = read_img
+read = read_img
+
+
+def write_img(img, filename):
+    """
+    Saves an image to a specified file.
+
+    The image format is chosen based on the filename extension
+
+    Parameters
+    ----------
+    img: Image to be saved
+
+    filename: Name of the file
+
+    Notes
+    -----
+    Only 8-bit single channel or 3-channel (BGR order) can be saved. If
+    the format, depth or channel order is different convert it first.
+
+    It is possible to store PNG images with an alpha channel using this
+    function. To do this, create 8-bit 4-channel image BGRA, where the alpha
+    channel goes last. Fully transparent pixels should have alpha set to 0,
+    fully opaque pixels should have alpha set to 255
+
+    """
+    cv2.imwrite(filename, img)
+
+
+save = write_img
+write = write_img
+
+
+def get_width_and_height(img):
+    """
+    Returns width, height for an image
+
+    Parameters
+    ----------
+    img: Array containing an image
+
+    Returns
+    -------
+    width: int
+        Width of the image
+    height: int
+        Height of the image
+
+    Notes
+    -----
+    Width of an image is the first dimension for numpy arrays.
+    Height of an image is the first dimension for openCV
+    """
+    width = get_width(img)
+    height = get_height(img)
+    return width, height
+
+
+dimensions = get_width_and_height
+
+
+def get_width(img):
+    """
+    Returns width for img
+
+    Parameters
+    ----------
+    img: Array containing an image
+
+    Returns
+    -------
+    width: int
+        Width of the image
+
+    """
+    return int(np.shape(img)[1])
+
+
+width = get_width
+
+
+def get_height(img):
+    """
+    Returns the height of an image
+
+    Parameters
+    ----------
+    img: Array containing an image
+
+    Returns
+    -------
+    height: int
+        height of the image
+
+    """
+    return int(np.shape(img)[0])
+
+
+height = get_height
+
+
+
+
+
+def get_depth(img):
+    shp = np.shape(img)
+    if len(shp) == 2:
+        return 1
+    else:
+        return shp[2]
---
+++ colors.py
@@ -0,0 +1,63 @@
+import cv2
+
+__all__ = [
+    'BLUE',
+    'LIME',
+    'RED',
+    'YELLOW',
+    'ORANGE',
+    'BLACK',
+    'WHITE',
+    'MAGENTA',
+    'PINK',
+    'CYAN',
+    'NAVY',
+    'TEAL',
+    'PURPLE',
+    'GREEN',
+    'MAROON',
+    'bgr_to_hsv',
+    'bgr_to_lab',
+    'hsv_to_bgr',
+    'lab_to_bgr',
+    'bgr_to_gray',
+    'gray_to_bgr',
+]
+
+BLUE = (255, 0, 0)
+LIME = (0, 255, 0)
+RED = (0, 0, 255)
+YELLOW = (0, 255, 255)
+ORANGE = (0, 128, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+MAGENTA = (255, 0, 255)
+PINK = MAGENTA
+CYAN = (255, 255, 0)
+NAVY = (128, 0, 0)
+TEAL = (128, 128, 0)
+PURPLE = (128, 0, 128)
+GREEN = (0, 128, 0)
+MAROON = (0, 0, 128)
+
+
+def bgr_to_hsv(im):
+    return cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+
+
+def bgr_to_lab(im):
+    return cv2.cvtColor(im, cv2.COLOR_BGR2LAB)
+
+
+def hsv_to_bgr(im):
+    return cv2.cvtColor(im, cv2.COLOR_HSV2BGR)
+
+
+def lab_to_bgr(im):
+    return cv2.cvtColor(im, cv2.COLOR_LAB2BGR)
+
+def bgr_to_gray(im):
+    return cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+def gray_to_bgr(im):
+    return cv2.cvtColor(im, cv2.COLOR_GRAY2BGR)
